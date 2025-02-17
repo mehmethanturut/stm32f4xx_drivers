c918e65005cbe031c655463bdf698f082c604ffe
Mehmethan Türüt <mehmetturut5353@gmail.com>
2025-02-17 12:17:47 +0300

    added TIM1 reg def

--- Drivers/Inc/stm32f401xx.h
+++ Drivers/Inc/stm32f401xx.h
@@ -1357,6 +1357,7 @@ volatile typedef struct {
 
 
 
+
 /* 
 ****************** EXTI register definition structures******************
 */
@@ -1876,6 +1877,251 @@ typedef struct
 
 }ADC_RegDef_t;
 
+
+/**
+ * @brief TIM1 Register Definition Structure
+ * 
+ * This structure represents the TIM1 peripheral registers.
+ */
+typedef struct {
+    /**
+     * @brief TIM1 Control Register 1 (TIMx_CR1)
+     */
+    typedef struct {
+        uint32_t CEN     :1;  /**< Counter enable */
+        uint32_t UDIS    :1;  /**< Update disable */
+        uint32_t URS     :1;  /**< Update request source */
+        uint32_t OPM     :1;  /**< One-pulse mode */
+        uint32_t DIR     :1;  /**< Direction */
+        uint32_t CMS     :2;  /**< Center-aligned mode selection */
+        uint32_t ARPE    :1;  /**< Auto-reload preload enable */
+        uint32_t CKD     :2;  /**< Clock division */
+        uint32_t res     :22; /**< Reserved */
+    } TIMx_CR1_t;
+
+    /**
+     * @brief TIM1 Control Register 2 (TIMx_CR2)
+     */
+    typedef struct {
+        uint32_t CCPC    :1;  /**< Capture/Compare preloaded control */
+        uint32_t res1    :1;  /**< Reserved */
+        uint32_t CCUS    :1;  /**< Capture/Compare control update selection */
+        uint32_t CCDS    :1;  /**< Capture/Compare DMA selection */
+        uint32_t MMS     :3;  /**< Master mode selection */
+        uint32_t TI1S    :1;  /**< TI1 selection */
+        uint32_t OIS1    :1;  /**< Output Idle state 1 */
+        uint32_t OIS1N   :1;  /**< Output Idle state 1 (complementary) */
+        uint32_t OIS2    :1;  /**< Output Idle state 2 */
+        uint32_t OIS2N   :1;  /**< Output Idle state 2 (complementary) */
+        uint32_t OIS3    :1;  /**< Output Idle state 3 */
+        uint32_t OIS3N   :1;  /**< Output Idle state 3 (complementary) */
+        uint32_t OIS4    :1;  /**< Output Idle state 4 */
+        uint32_t res2    :17; /**< Reserved */
+    } TIMx_CR2_t;
+
+    /**
+     * @brief TIM1 Slave Mode Control Register (TIMx_SMCR)
+     */
+    typedef struct {
+        uint32_t SMS     :3;  /**< Slave mode selection */
+        uint32_t res1    :1;  /**< Reserved */
+        uint32_t TS      :3;  /**< Trigger selection */
+        uint32_t MSM     :1;  /**< Master/slave mode */
+        uint32_t ETF     :4;  /**< External trigger filter */
+        uint32_t ETPS    :2;  /**< External trigger prescaler */
+        uint32_t ECE     :1;  /**< External clock enable */
+        uint32_t ETP     :1;  /**< External trigger polarity */
+        uint32_t res2    :16; /**< Reserved */
+    } TIMx_SMCR_t;
+
+    /**
+     * @brief TIM1 DMA/Interrupt Enable Register (TIMx_DIER)
+     */
+    typedef struct {
+        uint32_t UIE     :1;  /**< Update interrupt enable */
+        uint32_t CC1IE   :1;  /**< Capture/Compare 1 interrupt enable */
+        uint32_t CC2IE   :1;  /**< Capture/Compare 2 interrupt enable */
+        uint32_t CC3IE   :1;  /**< Capture/Compare 3 interrupt enable */
+        uint32_t CC4IE   :1;  /**< Capture/Compare 4 interrupt enable */
+        uint32_t COMIE   :1;  /**< COM interrupt enable */
+        uint32_t TIE     :1;  /**< Trigger interrupt enable */
+        uint32_t BIE     :1;  /**< Break interrupt enable */
+        uint32_t UDE     :1;  /**< Update DMA request enable */
+        uint32_t CC1DE   :1;  /**< Capture/Compare 1 DMA request enable */
+        uint32_t CC2DE   :1;  /**< Capture/Compare 2 DMA request enable */
+        uint32_t CC3DE   :1;  /**< Capture/Compare 3 DMA request enable */
+        uint32_t CC4DE   :1;  /**< Capture/Compare 4 DMA request enable */
+        uint32_t COMDE   :1;  /**< COM DMA request enable */
+        uint32_t TDE     :1;  /**< Trigger DMA request enable */
+        uint32_t res     :17; /**< Reserved */
+    } TIMx_DIER_t;
+
+    /**
+     * @brief TIM1 Status Register (TIMx_SR)
+     */
+    typedef struct {
+        uint32_t UIF     :1;  /**< Update interrupt flag */
+        uint32_t CC1IF   :1;  /**< Capture/Compare 1 interrupt flag */
+        uint32_t CC2IF   :1;  /**< Capture/Compare 2 interrupt flag */
+        uint32_t CC3IF   :1;  /**< Capture/Compare 3 interrupt flag */
+        uint32_t CC4IF   :1;  /**< Capture/Compare 4 interrupt flag */
+        uint32_t COMIF   :1;  /**< COM interrupt flag */
+        uint32_t TIF     :1;  /**< Trigger interrupt flag */
+        uint32_t BIF     :1;  /**< Break interrupt flag */
+        uint32_t res1    :1;  /**< Reserved */
+        uint32_t CC1OF   :1;  /**< Capture/Compare 1 overcapture flag */
+        uint32_t CC2OF   :1;  /**< Capture/Compare 2 overcapture flag */
+        uint32_t CC3OF   :1;  /**< Capture/Compare 3 overcapture flag */
+        uint32_t CC4OF   :1;  /**< Capture/Compare 4 overcapture flag */
+        uint32_t res2    :19; /**< Reserved */
+    } TIMx_SR_t;
+
+    /**
+     * @brief TIM1 Event Generation Register (TIMx_EGR)
+     */
+    typedef struct {
+        uint32_t UG      :1;  /**< Update generation */
+        uint32_t CC1G    :1;  /**< Capture/Compare 1 generation */
+        uint32_t CC2G    :1;  /**< Capture/Compare 2 generation */
+        uint32_t CC3G    :1;  /**< Capture/Compare 3 generation */
+        uint32_t CC4G    :1;  /**< Capture/Compare 4 generation */
+        uint32_t COMG    :1;  /**< Capture/Compare control update generation */
+        uint32_t TG      :1;  /**< Trigger generation */
+        uint32_t BG      :1;  /**< Break generation */
+        uint32_t res     :24; /**< Reserved */
+    } TIMx_EGR_t;
+
+    /**
+     * @brief TIM1 Capture/Compare Mode Register 1 (TIMx_CCMR1)
+     */
+    typedef struct {
+        uint32_t CC1S    :2;  /**< Capture/Compare 1 selection */
+        uint32_t OC1FE   :1;  /**< Output Compare 1 fast enable */
+        uint32_t OC1PE   :1;  /**< Output Compare 1 preload enable */
+        uint32_t OC1M    :3;  /**< Output Compare 1 mode */
+        uint32_t OC1CE   :1;  /**< Output Compare 1 clear enable */
+        uint32_t CC2S    :2;  /**< Capture/Compare 2 selection */
+        uint32_t OC2FE   :1;  /**< Output Compare 2 fast enable */
+        uint32_t OC2PE   :1;  /**< Output Compare 2 preload enable */
+        uint32_t OC2M    :3;  /**< Output Compare 2 mode */
+        uint32_t OC2CE   :1;  /**< Output Compare 2 clear enable */
+        uint32_t res     :16; /**< Reserved */
+    } TIMx_CCMR1_t;
+
+
+
+    /**
+     * @brief TIM1 Capture/Compare Mode Register 2 (TIMx_CCMR2)
+     */
+    typedef struct {
+        uint32_t CC3S    :2;  /**< Capture/Compare 3 selection */
+        uint32_t OC3FE   :1;  /**< Output Compare 3 fast enable */
+        uint32_t OC3PE   :1;  /**< Output Compare 3 preload enable */
+        uint32_t OC3M    :3;  /**< Output Compare 3 mode */
+        uint32_t OC3CE   :1;  /**< Output Compare 3 clear enable */
+        uint32_t CC4S    :2;  /**< Capture/Compare 4 selection */
+        uint32_t OC4FE   :1;  /**< Output Compare 4 fast enable */
+        uint32_t OC4PE   :1;  /**< Output Compare 4 preload enable */
+        uint32_t OC4M    :3;  /**< Output Compare 4 mode */
+        uint32_t OC4CE   :1;  /**< Output Compare 4 clear enable */
+        uint32_t res     :16; /**< Reserved */
+    } TIMx_CCMR2_t;
+
+
+    /**
+     * @brief TIM1 Capture/Compare Enable Register (TIMx_CCER)
+     */
+    typedef struct {
+        uint32_t CC1E    :1;  /**< Capture/Compare 1 output enable */
+        uint32_t CC1P    :1;  /**< Capture/Compare 1 output polarity */
+        uint32_t CC1NE   :1;  /**< Capture/Compare 1 complementary output enable */
+        uint32_t CC1NP   :1;  /**< Capture/Compare 1 complementary output polarity */
+        uint32_t CC2E    :1;  /**< Capture/Compare 2 output enable */
+        uint32_t CC2P    :1;  /**< Capture/Compare 2 output polarity */
+        uint32_t CC2NE   :1;  /**< Capture/Compare 2 complementary output enable */
+        uint32_t CC2NP   :1;  /**< Capture/Compare 2 complementary output polarity */
+        uint32_t CC3E    :1;  /**< Capture/Compare 3 output enable */
+        uint32_t CC3P    :1;  /**< Capture/Compare 3 output polarity */
+        uint32_t CC3NE   :1;  /**< Capture/Compare 3 complementary output enable */
+        uint32_t CC3NP   :1;  /**< Capture/Compare 3 complementary output polarity */
+        uint32_t CC4E    :1;  /**< Capture/Compare 4 output enable */
+        uint32_t CC4P    :1;  /**< Capture/Compare 4 output polarity */
+        uint32_t res     :18; /**< Reserved */
+    } TIMx_CCER_t;
+
+
+    /**
+     * @brief TIM1 Counter Register (TIMx_CNT)
+     */
+    typedef struct {
+        uint32_t CNT     :16; /**< Counter value */
+        uint32_t res     :16; /**< Reserved */
+    } TIMx_CNT_t;
+
+
+    /**
+     * @brief TIM1 Prescaler Register (TIMx_PSC)
+     */
+    typedef struct {
+        uint32_t PSC     :16; /**< Prescaler value */
+        uint32_t res     :16; /**< Reserved */
+    } TIMx_PSC_t;
+
+
+    /**
+     * @brief TIM1 Auto-Reload Register (TIMx_ARR)
+     */
+    typedef struct {
+        uint32_t ARR     :16; /**< Auto-reload value */
+        uint32_t res     :16; /**< Reserved */
+    } TIMx_ARR_t;
+
+
+
+    /**
+     * @brief TIM1 Capture/Compare Registers (TIMx_CCR1 - TIMx_CCR4)
+     */
+    typedef struct {
+        uint32_t CCR     :16; /**< Capture/Compare value */
+        uint32_t res     :16; /**< Reserved */
+    } TIMx_CCR1_t;
+
+
+    typedef struct {
+        uint32_t CCR     :16; /**< Capture/Compare value */
+        uint32_t res     :16; /**< Reserved */
+    } TIMx_CCR2_t;
+
+
+    typedef struct {
+        uint32_t CCR     :16; /**< Capture/Compare value */
+        uint32_t res     :16; /**< Reserved */
+    } TIMx_CCR3_t;
+
+
+    typedef struct {
+        uint32_t CCR     :16; /**< Capture/Compare value */
+        uint32_t res     :16; /**< Reserved */
+    } TIMx_CCR4_t;
+
+
+
+    /**
+     * @brief TIM1 Break and Dead-Time Register (TIMx_BDTR)
+     */
+    typedef struct {
+        uint32_t DTG     :8;  /**< Dead-time generator setup */
+        uint32_t LOCK    :2;  /**< Lock configuration */
+        uint32_t OSSI    :1;  /**< Off-state selection for Idle mode */
+        uint32_t OSSR    :1;  /**< Off-state selection for Run mode */
+        uint32_t BKE     :1;  /**< Break enable */
+        uint32_t BKP     :1;  /**< Break polarity */
+        uint32_t AOE     :1;  /**< Automatic output enable */
+        uint32_t MOE     :1;  /**< Main output enable */
+        uint32_t res     :16; /**< Reserved */
+    } TIMx_BDTR_t;
+
+} TIM1_RegDef_t;
 
 
 
